36c563e4956f535ec77450941512220ac1980cbc
Gammorin <karwoowski@yahoo.com>
2022-02-17 10:36:35 +0100

    save02
    change public to private

--- Project_01u/Assets/NewBehaviourScript.cs
+++ Project_01u/Assets/NewBehaviourScript.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        public int = 0;
+        private int o = 56;
     }
 
     // Update is called once per frame
